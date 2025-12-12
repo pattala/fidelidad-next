64e97e5bdad9feeba1c960833bcd060707fea9ec
pattala <pablo_attala@yahoo.com.ar>
2025-12-12 20:49:56 -0300

    fix: unignore and add config.js files for vercel deployment

---
+++ admin-panel/config.js
@@ -0,0 +1,9 @@
+window.ADMIN_CONFIG = {
+    platformName: "Club Demo Admin",
+    logoUrl: "./images/logo-empresa.png",
+    apiUrl: "https://fidelidad-api.vercel.app",
+    firebaseConfig: {},
+    featureFlags: {
+        useCreateUserAPI: true
+    }
+};
---
+++ client-pwa/config.js
@@ -0,0 +1,6 @@
+window.APP_CONFIG = {
+    appName: "Club Demo",
+    apiUrl: "https://fidelidad-api.vercel.app",
+    platformName: "Club Demo",
+    featureFlags: {}
+};

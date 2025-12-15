ca5f82c7000845ee9abe8abb1fb74b217d5651b0
pattala <pablo_attala@yahoo.com.ar>
2025-12-15 01:57:42 -0300

    deploy: v1.0.5 - FIXED address auto-open

--- client-pwa/app.js
+++ client-pwa/app.js
@@ -15,7 +15,7 @@ import {
 
 // === DEBUG / OBS ===
 window.__RAMPET_DEBUG = true;
-window.__BUILD_ID = 'pwa-2025-09-17-b';
+window.__BUILD_ID = 'pwa-1.0.5-fix-addr';
 function d(tag, ...args) { if (window.__RAMPET_DEBUG) console.log(`[DBG][${window.__BUILD_ID}] ${tag}`, ...args); }
 window.__reportState = async (where = '') => {
   const notifPerm = (window.Notification?.permission) || 'n/a';

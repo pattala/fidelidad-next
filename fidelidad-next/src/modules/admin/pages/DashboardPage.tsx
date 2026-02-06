04b34b1241e147413b8b62c00a4e45dd895d7871
pattala <pablo_attala@yahoo.com.ar>
2026-02-06 19:48:26 -0300

    Fix date simulation for birthday gifted check

--- fidelidad-next/src/modules/admin/pages/DashboardPage.tsx
+++ fidelidad-next/src/modules/admin/pages/DashboardPage.tsx
@@ -323,7 +323,7 @@ export const DashboardPage = () => {
                             </div>
                             <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
                                 {birthdaysOfToday.map(client => {
-                                    const currentYear = new Date().getFullYear().toString();
+                                    const currentYear = TimeService.now().getFullYear().toString();
                                     const alreadyGifted = client.lastBirthdayPointsYear === currentYear;
 
                                     return (

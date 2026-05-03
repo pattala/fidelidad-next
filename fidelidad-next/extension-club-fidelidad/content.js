5d3c47b03783ca04588783e42deedd84c8b44e6a
pattala <pablo_attala@yahoo.com.ar>
2026-05-03 01:47:47 -0300

    debug: add logs to extension alerts

--- fidelidad-next/extension-club-fidelidad/content.js
+++ fidelidad-next/extension-club-fidelidad/content.js
@@ -22,10 +22,12 @@ chrome.storage.local.get(['appName', 'apiUrl', 'apiKey', 'dismissedAlerts'], (re
             headers: { 'Content-Type': 'application/json', 'x-api-key': res.apiKey }
         }).then(r => r.json())
         .then(data => {
+                console.log("💎 [Club Fidelidad] Datos recibidos de API:", data);
                 // Mapeo robusto: el motor unificado devuelve arrays directamente (V.1.3.6-STABLE-REV2)
                 const bList = data.birthdays || [];
                 const eList = data.expirations || [];
                 const pList = data.petAlerts || [];
+                console.log("📊 [Club Fidelidad] Listas mapeadas -> Cumples:", bList.length, "Vencim:", eList.length, "Pets:", pList.length);
 
                 const dismissed = res.dismissedAlerts || [];
                 const filteredBirthdays = bList.filter(b => !dismissed.includes(`birthday-${b.socioNumber || b.dni}`));

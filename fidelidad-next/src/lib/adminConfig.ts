4e8e03624966ab0e57a51ce506b5965e66e1ef96
pattala <pablo_attala@yahoo.com.ar>
2026-05-05 19:44:13 -0300

    V.1.4.17: Final version synchronization

--- fidelidad-next/src/lib/adminConfig.ts
+++ fidelidad-next/src/lib/adminConfig.ts
@@ -1,7 +1,7 @@
 /**
  * APP VERSION & REVISION
  */
-export const APP_VERSION = 'V.1.4.16';
+export const APP_VERSION = 'V.1.4.17';
 
 /**
  * CONFIGURACIÓN DE SEGURIDAD ADMINISTRATIVA (White Label)

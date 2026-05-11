d5d5e43ce3f1eb5b2054daa56a8888de588227e5
pattala <pablo_attala@yahoo.com.ar>
2026-05-10 22:46:28 -0300

    Bump version to V.1.4.60

--- fidelidad-next/src/lib/adminConfig.ts
+++ fidelidad-next/src/lib/adminConfig.ts
@@ -1,7 +1,7 @@
 /**
  * APP VERSION & REVISION
  */
-export const APP_VERSION = 'V.1.4.59';
+export const APP_VERSION = 'V.1.4.60';
 
 /**
  * CONFIGURACIÓN DE SEGURIDAD ADMINISTRATIVA (White Label)

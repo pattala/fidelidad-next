15160d172bc447cd54b8c1773280400134c5c753
pattala <pablo_attala@yahoo.com.ar>
2026-05-14 23:25:23 -0300

    V.1.4.86: Re-uploading campaign fixes to desarrollo

--- fidelidad-next/src/lib/adminConfig.ts
+++ fidelidad-next/src/lib/adminConfig.ts
@@ -1,7 +1,7 @@
 /**
  * APP VERSION & REVISION
  */
-export const APP_VERSION = 'V.1.4.85';
+export const APP_VERSION = 'V.1.4.86';
 
 /**
  * CONFIGURACIÓN DE SEGURIDAD ADMINISTRATIVA (White Label)

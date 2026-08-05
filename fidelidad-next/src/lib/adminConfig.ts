2883713ad3cecab10e98f6fd67ed7690ea1c085c
pattala <pablo_attala@yahoo.com.ar>
2026-08-05 01:37:07 -0300

    v1.6.79: Add button explanatory hover tooltips in web installer UI

--- fidelidad-next/src/lib/adminConfig.ts
+++ fidelidad-next/src/lib/adminConfig.ts
@@ -1,7 +1,7 @@
 /**
  * APP VERSION & REVISION
  */
-export const APP_VERSION = 'V.1.6.78';
+export const APP_VERSION = 'V.1.6.79';
 
 /**
  * CONFIGURACIÓN DE SEGURIDAD ADMINISTRATIVA (White Label)
